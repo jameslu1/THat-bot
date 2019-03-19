c8808eaec0dbf990e7a674224eefb71ca014f8b7
James Lu <jameslu@utexas.edu>
2019-03-19 16:28:34 -0500

    bot 1

---
+++ script.py
@@ -0,0 +1,22 @@
+import praw
+import pdb
+import re
+import os
+
+def main():
+    #initialize bots
+    r1 = praw.Reddit('bot1')
+    subreddit = r1.subreddit("all")
+
+    for comment in subreddit.stream.comments(skip_existing=True):
+        if re.search("!hoovaq", comment.body, re.IGNORECASE):
+            reply = ""
+            if comment.is_root:
+                reply = comment.parent().title + " | " + comment.parent().title.replace("h","H")
+            else:
+                reply = comment.parent().body + " | " + comment.parent().body.replace("h","H")
+            comment.reply(reply)
+            print("Comment: " + comment.body)
+
+if __name__=="__main__":
+    main()
